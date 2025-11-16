5cefca366ccc6ee068bbf8e85848e080789b7b7b
Sertuğ <sertugrate@outlook.com>
2025-11-16 12:35:59 +0300

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,45 @@
+local function sertugluzzy()
+    while true do
+        while true do
+            while true do
+                while true do
+                    while true do
+                        while true do
+                            while true do
+                                while true do
+                                    while true do
+                                        while true do
+                                            while true do
+                                                while true do
+                                                    while true do
+                                                        while true do
+                                                            while true do
+                                                                while true do
+                                                                    while true do
+                                                                        while true do
+                                                                            while true do
+                                                                                while true do
+                                                                                    -- print("bonaparte")
+                                                                                end
+                                                                            end
+                                                                        end
+                                                                    end
+                                                                end
+                                                            end
+                                                        end
+                                                    end
+                                                end
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+sertugluzzy()
